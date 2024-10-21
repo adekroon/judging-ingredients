f81456b25a942f6f775da372d858fd7e668bea59
adekroon <ammdekroon@tudelft.student.nl>
2024-10-21 16:52:11 +0200

    removed test functions

--- judging_ingredients_functions.py
+++ judging_ingredients_functions.py
@@ -15,18 +15,16 @@ def check_flour_quantity(num):
     str : decision/judgement if flour quantity is sufficient to make bread 
 
     """
+    if isinstance(num, str):
+        return "String input not accepted"
+    
     if num == 500:
         return 'correct flour quantity'
     elif num > 500:
         return 'too much flour. you should use 500g of flour'
     else:
         return 'too little flour. you should use 500g of flour'
-
-
-def test_check_flour_quantity():
-    assert check_flour_quantity(500) == 'correct flour quantity'
-    assert check_flour_quantity(' ') == 'too little flour. you should use 500g of flour'
-test_check_flour_quantity()     
+   
 
 def check_flour_type(flour_type):
     """
@@ -45,10 +43,7 @@ def check_flour_type(flour_type):
     else: 
         return "this flour type shouldn't be used. you should use all-purpose flour, bread flour, whole wheat flour, or white whole wheat flour"
     
-    
-def test_check_flour_type():
-    assert check_flour_type('white flour')=="this flour type shouldn't be used"
-test_check_flour_type()  
+  
 
 def check_water_quantity(num):
     """
@@ -67,10 +62,7 @@ def check_water_quantity(num):
         return 'too much water. you should use 300ml of water'
     else: 
         return 'too little water. you should use 300ml of water'
-    
-def test_check_water_quantity():
-    assert check_water_quantity(400)=='too much water. you should use 300ml of water'
-test_check_water_quantity()
+
 
 def check_milk_quantity(num):
     """
@@ -88,9 +80,7 @@ def check_milk_quantity(num):
     else: 
         return "don't need milk"
 
-def test_check_milk_quantity():
-    assert check_milk_quantity(100)=="don't need milk"
-test_check_milk_quantity()
+
 
 def check_salt_quantity(num):
     """
@@ -110,9 +100,7 @@ def check_salt_quantity(num):
     else: 
         return 'too little salt. you should use 2 tsp of salt'
     
-def test_check_salt_quantity():
-    assert check_salt_quantity(1)=='too little salt. you should use 2 tsp of salt'
-test_check_salt_quantity()
+
 
 def check_yeast_quantity(num):
     """
@@ -132,9 +120,7 @@ def check_yeast_quantity(num):
     else: 
         return 'too little yeast. you should use 7g of yeast'
 
-def test_check_yeast_quantity():
-    assert check_yeast_quantity(6)=='too little yeast. you should use 7g of yeast'
-test_check_yeast_quantity()
+
     
 def check_oil_quantity(num):
     """
@@ -154,9 +140,7 @@ def check_oil_quantity(num):
     else:
         return 'too little oil. you should use 3 tbs of oil'
     
-def test_check_oil_quantity():
-    assert check_oil_quantity(4)=='too much oil. you should use 3 tbs of oil'
-test_check_oil_quantity()
+
     
 def check_oil_type(oil_type):
     """
@@ -175,9 +159,7 @@ def check_oil_type(oil_type):
     else: 
         return "this oil type shouldn't be used. you should use olive oil or coconut oil"
     
-def test_check_oil_type():
-    assert check_oil_type('vegetable oil')== "this oil type shouldn't be used. you should use olive oil or coconut oil"
-test_check_oil_type()
+
 
 def check_egg_quantity(num):
     """
@@ -195,9 +177,7 @@ def check_egg_quantity(num):
     else:
         return "don't need eggs"
     
-def test_check_egg_quantity():
-    assert check_egg_quantity(1)=="don't need eggs"
-test_check_egg_quantity()
+
 
 
 
